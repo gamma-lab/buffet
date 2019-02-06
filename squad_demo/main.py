2b5e8fd52ca5612f89051608e168aa8340025155
Surya Kasturi <kasturisurya@gmail.com>
2019-02-06 15:38:22 -0500

    Add files via upload

---
+++ squad_demo/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from squad import qa_system_predict
+
+
+app = Flask("SQuAD")
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    context = {}
+    if request.method == "POST":
+        answer_text = qa_system_predict(
+            request.form["paragraph"],
+            request.form["question"])
+        context = dict(
+            question=request.form["question"],
+            paragraph=request.form["paragraph"],
+            answer=answer_text)
+    return render_template("index.html", **context)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8881, debug=True)
